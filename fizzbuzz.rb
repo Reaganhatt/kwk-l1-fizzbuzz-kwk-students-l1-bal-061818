5c8d7bdc9a177c2e83ba6002604acce5fbd35e29
Reaganhatt <reagan.hatt@gmail.com>
2018-06-19 19:25:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,13 @@
 def fizz_buzz(int)
   if int%3 == 0 
     puts "Fizz"
+  if int%5 == 0 
+    puts "Buzz"
+  if int%15 == 0 
+    puts "FizzBuzz"
+  else 
+    puts "nil"
   end
 end
 
-fizz_buzz(3)
+fizz_buzz()
